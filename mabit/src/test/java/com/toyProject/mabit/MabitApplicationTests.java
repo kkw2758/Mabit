94bfd3a04d39318d562a5435b5713c0bccd34af6
Sehyeon <dtc3229@gmail.com>
2023-04-11 15:10:30 +0900

    first commit

---
+++ mabit/src/test/java/com/toyProject/mabit/MabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyProject.mabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
